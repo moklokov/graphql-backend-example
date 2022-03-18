bae7f108beaeea3eb4168d7c073cfd460716e437
moklokov <moklokov.kostya@gmail.com>
2022-03-18 16:02:45 +0200

    feat(feed): add query to get links list

---
+++ src/graphql/Link.ts
@@ -0,0 +1,23 @@
+import { objectType, extendType } from "nexus";
+import { links } from "../fixtures/links";
+
+export const Link = objectType({
+  name: "Link",
+  definition(t) {
+    t.nonNull.int("id");
+    t.nonNull.string("description");
+    t.nonNull.string("url");
+  },
+});
+
+export const LinkQuery = extendType({
+  type: "Query",
+  definition(t) {
+    t.nonNull.list.nonNull.field("feed", {
+      type: "Link",
+      resolve(parent, args, context, info) {
+        return links;
+      },
+    });
+  },
+});
